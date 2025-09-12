43b8b3856f7913caa7dc29a7d32a0f8692e94a2d
Anissshh <rfanish666@gmail.com>
2025-09-13 01:36:56 +0530

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -1,10 +1,3 @@
-@ECHO OFF
-
-SET DIR=%~dp0
-SET APP_HOME=%DIR%
-SET APP_BASE_NAME=%~n0
-SET APP_NAME=Gradle
-
-SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-java %DEFAULT_JVM_OPTS% -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+@echo off
+set DIR=%~dp0
+"%DIR%\gradlew" %*
